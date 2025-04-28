860013843235a37b8dcdb1659c475be4933dc62b
Eman <emanuelbautista354@gmail.com>
2025-04-28 11:45:34 +0800

    second commit

--- semifinals_bautista/loginpage/views.py
+++ semifinals_bautista/loginpage/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def logs(request):
+    return render(request,'loginpage.html'),
--- semifinals_bautista/semifinals_bautista/urls.py
+++ semifinals_bautista/semifinals_bautista/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from loginpage.views import logs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vmg', logs),
+    
 ]
